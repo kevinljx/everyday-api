c3dd85699cfaa574d6574c2ec06645dacf98621f
kevinljx <kevin.ljx@gmail.com>
2019-05-24 08:37:21 +0800

    Add files via upload

--- common/models/lead.js
+++ common/models/lead.js
@@ -1,5 +1,24 @@
 'use strict';
 
 module.exports = function(Lead) {
-    
+   
+   Lead.showFullAddress = function showFullAddress(lead){
+     var address = "";
+     if(lead.address_1){
+         address += lead.address_1+"\n";         
+     }
+     if(lead.address_2){
+        address += lead.address_2+"\n";         
+     }
+     if(lead.state){
+        address += lead.state+",";         
+     }
+     if(lead.city){
+        address += lead.city+" ";         
+     }
+     if(lead.zip){
+        address += lead.zip;         
+     }
+     return address;
+   };
 };
