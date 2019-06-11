2b7d12d540ee880a223b516fbeb526981574de6f
jenglim <limjeng@ocdigitalnetwork.com>
2019-06-11 17:21:31 +0800

    api call refactor

--- common/models/access-role.js
+++ common/models/access-role.js
@@ -1,5 +1,27 @@
 'use strict';
 
 module.exports = function(Accessrole) {
-
+  Accessrole.getAllRoleRights = async function() {
+    var roles = await Accessrole.app.models.AccessRole.find()
+    var rolesRights = []
+    for (let i = 0; i < roles.length; i++) {
+      var rights = await roles[i].accessRights.find()
+      var accessRights = {
+        roleID: roles[i].id,
+        rights: rights
+      }
+      rolesRights.push(accessRights)
+    }
+    try {
+        return [rolesRights];
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
+  }
+  
+  Accessrole.remoteMethod('getAllRoleRights', {
+    returns: {arg: 'data', type: 'array'},
+    http: {verb: 'get'}
+  });
 };
