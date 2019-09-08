4f4c19972106879c6bd29c61b1af34b3725741f6
Kevin Low <kevin.ljx@gmail.com>
2019-09-09 00:09:38 +0800

    fixes to signup access

--- common/models/base-user.js
+++ common/models/base-user.js
@@ -23,7 +23,7 @@ module.exports = function (Baseuser) {
     var options = {
       type: "email",
       to: user.baseContact.email,
-      from: "Ester from Everyday <hello@everydaycrm.sg>",
+      from: "Esther from Everyday <hello@everydaycrm.sg>",
       subject: "Please verify your email address",
       template: path.resolve(__dirname, "../../server/views/verify.ejs"),
       redirect: `/verified`,
@@ -205,4 +205,217 @@ module.exports = function (Baseuser) {
     return;
   });
   */
+
+ Baseuser.signup = async function (
+  email,
+  password,
+  priceplan,
+  userInfo,
+  companyInfo,
+  paymentInfo
+) {
+  //check if user already signed up with same email address
+  //var BaseUser = Company.app.models.BaseUser;
+  var Company = Baseuser.app.models.BaseCompany;
+  try {
+    var users = await Baseuser.find({ where: { email: email } });
+    if (users.length > 0) {
+      var error = new Error(
+        "A user has already registered with this email address."
+      );
+      error.status = 400;
+      throw error;
+    } else {
+      //get priceplan
+      var Priceplan = Baseuser.app.models.PricePlan;
+      var pp = await Priceplan.findOne({ name: priceplan });
+
+      if (pp == undefined || pp == null) {
+        var error = new Error("Invalid price plan.");
+        error.status = 400;
+        throw error;
+      }
+      //company info
+      if (companyInfo !== undefined) {
+        companyInfo.pricePlanId = pp.id;
+        var comp = await Company.create(companyInfo);
+      } else {
+        var comp = await Company.create({
+          name: "Company",
+          pricePlanId: pp.id,
+          contact: {
+            email: email
+          }
+        });
+      }
+
+      // user info
+      // { firstName: 'Hello', lastName: 'World' }
+      var name = userInfo.firstName + " " + userInfo.lastName;
+      //console.log(userInfo);
+      if (userInfo == null) {
+        userInfo = { email: email };
+      } else {
+        if (!userInfo.hasOwnProperty("email")) {
+          userInfo.email = email;
+        }
+        if (userInfo.hasOwnProperty("name")) {
+          name = userInfo.firstName + " " + userInfo.lastName;;
+        }
+      }
+
+      var newuser = await Baseuser.create({
+        name: name,
+        email: email,
+        password: password,
+        baseContact: userInfo,
+        company: comp
+      });
+
+
+      comp.paymentInfos.create(paymentInfo);
+
+      //create default access rights
+      var AccessGroup = Baseuser.app.models.AccessGroup;
+      var companyGroup = await AccessGroup.create({
+        name: "Company",
+        userId: newuser.id
+      });
+      var AccessGroupRole = Baseuser.app.models.AccessGroupRole;
+      var AccessSetting = Baseuser.app.models.AccessSetting;
+      var AccessRole = Baseuser.app.models.AccessRole;
+      var pRoles = await pp.defaultRoles.find();
+
+      //duplicate the role for company
+      pRoles.forEach(async element => {
+        var roleRights = await element.accessRights.find();
+        var r1 = await AccessRole.create({
+          name: element.name,
+          userId: newuser.id
+        });
+        roleRights.forEach(async right1 => {
+          r1.accessRights.add(right1);
+        });
+        if (element.userId == "defaultAdmin") {
+          var grouprole = await AccessGroupRole.create({
+            tier: 3,
+            isDefault: true,
+            accessGroupId: companyGroup.id,
+            accessRoleId: r1.id
+          });
+          AccessSetting.create({ user: newuser, grouprole: grouprole });
+        }
+      });
+
+      //copy leads and deals stuff
+      var LeadStatus = Baseuser.app.models.LeadStatus;
+      var LeadSource = Baseuser.app.models.LeadSource;
+      var LeadInterest = Baseuser.app.models.LeadInterestLevel;
+      var LeadIndustry = Baseuser.app.models.LeadIndustry;
+      var DealType = Baseuser.app.models.DealType;
+      var DealStage = Baseuser.app.models.DealStage;
+      var statuses = await LeadStatus.find({ where: { userId: "default" } });
+      statuses.forEach(async element => {
+        LeadStatus.create({
+          name: element.name,
+          color: element.color,
+          userId: newuser.id
+        });
+      });
+      var sources = await LeadSource.find({ where: { userId: "default" } });
+      sources.forEach(async element => {
+        LeadSource.create({
+          name: element.name,
+          color: element.color,
+          userId: newuser.id
+        });
+      });
+      var interests = await LeadInterest.find({ where: { userId: "default" } });
+      interests.forEach(async element => {
+        LeadInterest.create({
+          name: element.name,
+          level: element.level,
+          userId: newuser.id
+        });
+      });
+      var industries = await LeadIndustry.find({ where: { userId: "default" } });
+      industries.forEach(async element => {
+        LeadIndustry.create({
+          name: element.name,
+          userId: newuser.id
+        });
+      });
+      var dealtypes = await DealType.find({ where: { userId: "default" } });
+      dealtypes.forEach(async element => {
+        DealType.create({
+          name: element.name,
+          color: element.color,
+          userId: newuser.id
+        });
+      });
+      var dealstages = await DealStage.find({ where: { userId: "default" } });
+      dealstages.forEach(async element => {
+        DealStage.create({
+          name: element.name,
+          chance: element.chance,
+          color: element.color,
+          step: element.step,
+          invoice: element.invoice,
+          quotation: element.quotation,
+          description: element.description,
+          userId: newuser.id
+        });
+      });
+
+      return [1, "Account created.", newuser];
+    }
+  } catch (e) {
+    throw e;
+  }
+};
+
+Baseuser.remoteMethod("signup", {
+  accepts: [
+    { arg: "email", type: "string", required: true },
+    { arg: "password", type: "string", required: true },
+    { arg: "priceplan", type: "string", required: true },
+    { arg: "userInfo", type: "object" },
+    { arg: "companyInfo", type: "object" },
+    { arg: "paymentInfo", type: "object" }
+  ],
+  returns: [
+    { arg: "success", type: "number" },
+    { arg: "msg", type: "string" },
+    { arg: "newuser", type: "object" }
+  ]
+});
+
+Baseuser.afterRemote("signup", function (context, user, next) {
+  //var BaseUser = Company.app.models.BaseUser;
+
+  var options = {
+    type: "email",
+    to: context.args.email,
+    // to : `igc14.gianjie@gmail.com`,
+    from: "Esther from Everyday <hello@everydaycrm.sg>",
+    subject: "Thank you for registering",
+    template: path.resolve(__dirname, "../../server/views/verify.ejs"),
+    redirect: `/verified`,
+    user: Baseuser,
+    // host: 'api.everydaycrm.sg'
+  };
+
+  user.newuser.verify(options, function (err, response) {
+    if (err) {
+      console.log('err')
+      // Prevent Spam Accounts
+      // BaseUser.deleteById(user.id);
+      return next(err);
+    }
+
+    // No error, send new user email verification
+  });
+
+  next();
+});
 };
